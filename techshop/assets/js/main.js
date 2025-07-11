b6195d535ebe2fd1765321e652be6afd7be5f61f
Brian Gill <71089300+brigill@users.noreply.github.com>
2025-07-11 12:13:53 -0400

    Update main.js

--- techshop/assets/js/main.js
+++ techshop/assets/js/main.js
@@ -7,4 +7,13 @@ document.addEventListener("DOMContentLoaded", function () {
     navToggle.addEventListener("click", function () {
         navMenu.classList.toggle("active");
     });
+
+    const score-a = 2;
+    const score-b = 3;
+    const score-c = (score-a + score-b);
+
+$("#nav-toggle").click(function() {
+   $(this).css('background', 'red');                    
+});
+    
 });
